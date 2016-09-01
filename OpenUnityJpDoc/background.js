716b77631bc9bfa0072dc4e17e84caefefb3d8ba
sekig <mkt.sekiguchi@gmail.com>
2016-09-01 16:38:35 +0900

    add internet url convert

--- OpenUnityJpDoc/background.js
+++ OpenUnityJpDoc/background.js
@@ -7,6 +7,13 @@ chrome.browserAction.onClicked.addListener(function()
 				url: newurl
 			});
 		}
+		else if (tab.url.match(/^(?!.*\/ja\/).*(?=http:\/\/docs\.unity3d\.com\/ScriptReference).*$/i))
+		{
+			var newurl = tab.url.replace(/^http:\/\/docs\.unity3d\.com\/S/i, 'http://docs.unity3d.com/ja/current/S');
+			chrome.tabs.update({
+				url: newurl
+			});
+		}
 	});
 });
 
